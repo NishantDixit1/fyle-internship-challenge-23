3e908efd17ce38600d8947d4e526d4f184d3ffc9
Nishant Dixit <125440575+NishantDixit1@users.noreply.github.com>
2024-02-15 13:20:44 +0530

    Update skeleton.component.ts

--- src/app/Component/skeleton/skeleton.component.ts
+++ src/app/Component/skeleton/skeleton.component.ts
@@ -1,1 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton',
+  templateUrl: './skeleton.component.html',
+  styleUrls: ['./skeleton.component.scss']
+})
+export class SkeletonComponent {
+  @Input() isLoaded!:boolean;
+constructor(){}
+}
 
