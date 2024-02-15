4577ed27bc1914c1df59d562970bbc45fc085a25
Nishant Dixit <125440575+NishantDixit1@users.noreply.github.com>
2024-02-15 13:20:23 +0530

    Update skeleton.component.spec.ts

--- src/app/Component/skeleton/skeleton.component.spec.ts
+++ src/app/Component/skeleton/skeleton.component.spec.ts
@@ -1,1 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SkeletonComponent } from './skeleton.component';
+
+describe('SkeletonComponent', () => {
+  let component: SkeletonComponent;
+  let fixture: ComponentFixture<SkeletonComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [SkeletonComponent]
+    });
+    fixture = TestBed.createComponent(SkeletonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
 
